5d2296ae8ffef954ad3152d7bae0be55e9a1a0b4
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2023-11-25 19:01:52 +0800

    更新 repo.ts

--- src/common/api/repo.ts
+++ src/common/api/repo.ts
@@ -65,13 +65,6 @@ export const initEmptyRepo = async (
   showTips: boolean = true
 ) => {
   const README = `
-# Welcome to use PicX
-
-[PicX](https://github.com/XPoet/picx) is a simple and powerful image hosting tool. It supports image hosting services via GitHub repository.
-
-PicX is completely open source, and you can use it for free.
-
-If you like it, please give it a star on [GitHub](https://github.com/XPoet/picx).
         `
   const { owner, selectedRepo: repo, selectedBranch: branch } = userConfigInfo
 
