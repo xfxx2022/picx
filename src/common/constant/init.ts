beaf3c741359098db5c3ce1f369fc023c38e36ba
薰风习习 <25678988+xfxx2022@users.noreply.github.com>
2023-11-25 19:03:01 +0800

    更新 init.ts

--- src/common/constant/init.ts
+++ src/common/constant/init.ts
@@ -1,5 +1,5 @@
 export const PICX_REPO_NAME = 'XPoet/picx'
-export const INIT_REPO_NAME = 'picx-images-hosting'
+export const INIT_REPO_NAME = 'images-hosting'
 export const INIT_REPO_DESC = 'PicX images hosting repository'
 export const INIT_REPO_BARNCH = 'master'
 export const PICX_UPLOAD_IMG_DESC = 'Upload image via PicX(https://github.com/XPoet/picx)'
